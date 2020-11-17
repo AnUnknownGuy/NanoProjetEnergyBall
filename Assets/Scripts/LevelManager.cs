76c1cbd1888defafdf5d744a3cd41a87b6ee54b9
anunknownguy <warion.hugo@gmail.com>
2020-11-17 11:55:11 +0100

    added levelmanager, ui, health decay

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public Slider healthPlayer1;
+    public Slider healthPlayer2;
+    public Player player1;
+    public Player player2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthPlayer1.maxValue = player1.health;
+        healthPlayer2.maxValue = player2.health;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        healthPlayer1.value = player1.health;
+        healthPlayer2.value = player2.health;
+    }
+}
