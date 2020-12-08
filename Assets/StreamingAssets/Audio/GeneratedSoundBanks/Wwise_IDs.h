cdb7e52fb3fb59fff06dd501ce94ac9206871e0c
Clément <rougclem@gmail.com>
2020-12-08 15:25:25 +0100

    Inte_Wwise

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,58 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AIR_VELOCITY = 2652654800U;
+        static const AkUniqueID PLAY_BATTLE_MUSIC = 2862166672U;
+        static const AkUniqueID PLAY_BEGIN = 2375588297U;
+        static const AkUniqueID PLAY_BOUNCE = 3465618802U;
+        static const AkUniqueID PLAY_DASH_HAUG = 223895672U;
+        static const AkUniqueID PLAY_DASH_HIT = 2481879868U;
+        static const AkUniqueID PLAY_DASH_RUUD = 2666681873U;
+        static const AkUniqueID PLAY_FIGHT_ANNOUNCEMENT = 2301463414U;
+        static const AkUniqueID PLAY_GET = 2354553126U;
+        static const AkUniqueID PLAY_HORN = 2956245481U;
+        static const AkUniqueID PLAY_JUMP = 3689126666U;
+        static const AkUniqueID PLAY_MENU_MUSIC = 2228153899U;
+        static const AkUniqueID PLAY_NAVIGATE = 3893482503U;
+        static const AkUniqueID PLAY_THROW = 1977644338U;
+        static const AkUniqueID PLAY_THROW_HIT = 3286906532U;
+        static const AkUniqueID PLAY_VALIDATE = 1648095864U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BALL_VELOCITY = 145148864U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SB_MASTER = 1579730687U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
