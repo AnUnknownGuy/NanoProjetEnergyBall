344459ec4d7a822230c3c83afdaeb3789ddb19ca
Baptiste Rousserie <b.rousserie24@gmail.com>
2020-12-11 19:35:36 +0100

    WIP: Intégration VFX (Prefabs Gaelle)

---
+++ Assets/Scripts/VFXManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXManager : MonoBehaviour
+{
+    public static VFXManager Instance;
+    
+    [Header("VFX Prefabs")]
+    public GameObject Dash;
+    public GameObject Run;
+    public GameObject Jump;
+    public GameObject FallImpact;
+    public GameObject BlueExplosion;
+    public GameObject GreenExplosion;
+
+    void Start()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(this);
+    }
+    
+    
+}
