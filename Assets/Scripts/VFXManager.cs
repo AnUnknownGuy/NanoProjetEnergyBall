ad898e6181751ba51c83a30a6db8bb96b98dff64
Baptiste Rousserie <b.rousserie24@gmail.com>
2020-12-12 00:05:57 +0100

    WIP: Intégration VFX (Prefabs et scripts VFX)

--- Assets/Scripts/VFXManager.cs
+++ Assets/Scripts/VFXManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class VFXManager : MonoBehaviour
 {
@@ -13,12 +14,26 @@ public class VFXManager : MonoBehaviour
     public GameObject FallImpact;
     public GameObject BlueExplosion;
     public GameObject GreenExplosion;
+    public GameObject BallImpact;
 
     void Start()
     {
         if (Instance == null) Instance = this;
         else Destroy(this);
     }
-    
-    
+
+    public static void Spawn(GameObject prefab, Vector2 pos, bool flip)
+    {
+        Spawn(prefab, pos).GetComponent<SpriteRenderer>().flipX = flip;
+    }
+
+    public static void Spawn(GameObject prefab, Vector2 pos, Color color)
+    {
+        Spawn(prefab, pos).GetComponent<VisualEffect>().SetVector4("Color", color); 
+    }
+
+    public static GameObject Spawn(GameObject prefab, Vector2 pos)
+    {
+        return Instantiate(prefab, pos, Quaternion.identity);
+    }
 }
