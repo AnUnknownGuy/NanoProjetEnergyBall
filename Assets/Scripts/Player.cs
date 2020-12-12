5fe80b6ed77cd9f362ef021fde63009f09b43565
anunknownguy <warion.hugo@gmail.com>
2020-12-12 14:01:54 +0100

    visual prefab, integration dans Level

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -97,7 +97,6 @@ public class Player : MonoBehaviour
         AnimJump(isJumping);
         AnimFalling(!onGround);
         AnimRecovery(onGround);
-        Debug.Log(rb.gravityScale);
     }
 
     public void Dash() {
