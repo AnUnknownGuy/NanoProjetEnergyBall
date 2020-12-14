b0dc9ba96a5632b8300c057d0cea78b1fdf7b594
Baptiste Rousserie <b.rousserie24@gmail.com>
2020-12-14 01:44:43 +0100

    Integrated navigation sound in UI

---
+++ Assets/Scripts/SelectionNoise.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SelectionNoise : MonoBehaviour, ISelectHandler
+{
+    public void OnSelect(BaseEventData eventData)
+    {
+        AudioManager.Navigate(gameObject);
+    }
+}
